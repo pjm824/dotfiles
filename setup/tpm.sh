9f4b48dc4d916a905714ee04e42b89a7434e1a9c
pjm824 <pauljmin@gmail.com>
2021-05-21 17:24:27 -0700

    reorganize

--- setup/tpm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d ~/.tmux/plugins/tpm ]
-then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-else
-  echo "~/.tmux/plugins/tpm already exists.  not installing..."
-fi
